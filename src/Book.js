22d37e633cd05b6d17c062d8bbc65d85b2ea0dee
Ewelina <ewlaaa@gmail.com>
2017-11-28 00:31:50 +0100

    refactor: Extract components, Show data from API

---
+++ src/Book.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ShelfControl from './ShelfControl';
+
+const Book = ({ book }) => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+      <ShelfControl />
+      </div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors}</div>
+    </div>
+  )
+};
+
+export default Book;
---
+++ src/ListBooks.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import BookShelf from './BookShelf';
+
+class ListBooks extends React.Component {
+  state = {
+    books: []
+  };
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books })
+    })
+  }
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf
+              label="Currently Reading"
+              books={this.state.books.filter((book) => book.shelf === 'currentlyReading')}
+            />
+            <BookShelf
+              label="Want to Read"
+              books={this.state.books.filter((book) => book.shelf === 'wantToRead')}
+            />
+            <BookShelf
+              label="Read"
+              books={this.state.books.filter((book) => book.shelf === 'read')}
+            />
+          </div>
+        </div>
+        <div className="open-search">
+          <Link
+            // onClick={() => this.setState({ showSearchPage: true })}
+            to="/search"
+          >
+            Add a book
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBooks;
